6d05b6cce326b614734b3a9c7d2aaa9eb42e34eb
busramozdemir <51775266+busramozdemir@users.noreply.github.com>
2023-07-03 10:54:48 -0600

    some nessesscarry updates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="HomePage">
+      <header className="HopePage-header">
+        <img src={logo} className="HomePage-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="HomePage-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
